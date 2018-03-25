aef1456d39d069cddaaa0e09a3920930fd5ae483
Nikolay <meta1-heart@yandex.ru>
2018-03-26 01:10:03 +0500

    Второй коммит (супер кнопка)
    -Экспоненциальное распределение для времени начала задач
    -Визуализация решения задач (ошибка: иногда задачи могут накладываться друг на друга)

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var canvas, context;
 var HEIGHT = window.innerHeight, WIDTH = window.innerWidth;
 
-document.addEventListener("DOMContentLoaded", main, true);
+//document.addEventListener("DOMContentLoaded", main, true);
 
 
 var tasks = new Array();
-var tasksAmount = 30;
+var tasksAmount = 50;
 var size = 10;
 var expectationLenght = 15;
+var lambda = 5000; // task coming intensivity 
+var solvingTime = 0.5; // in seconds
 var sigma = 3;
 var multiplier = 1;
+var isStarted = false;
 
 
 function Task() {
@@ -22,24 +25,45 @@ function Task() {
 
 
 function main(){
-        canvas = document.createElement('canvas');
-        canvas.height = HEIGHT;
-        canvas.width = WIDTH;
-        document.body.appendChild(canvas);
-        context = canvas.getContext("2d");
-        
-        for(let i = 0; i < tasksAmount; i++) {
-            let aTask = new Task();
-            aTask.x = size * i * 2;
-            aTask.y = size * i * 2;
-            aTask.L = Math.round(multiplier * GaussRand(expectationLenght, sigma));
-            aTask.number = i;
-            tasks.push(aTask);
+        if (!isStarted) {
+            prepareCanvas();
+            createFirstTasks();
+            Draw();
+            isStarted = true;
         }
-        Draw();
+        let timer = setInterval(oneIterationStep, solvingTime * 1000);
 }
 
-function GaussRand(Mx, sigma) { // Box-Muller
+function prepareCanvas() {
+    canvas = document.createElement('canvas');
+    canvas.height = HEIGHT;
+    canvas.width = WIDTH;
+    document.body.appendChild(canvas);
+    context = canvas.getContext("2d");
+}
+
+function createFirstTasks() {
+    for(let i = 0; i < tasksAmount; i++) {
+        let aTask = new Task();
+        aTask.t0 = expTime(lambda);
+        aTask.x = aTask.t0;
+        aTask.y = Math.random() * HEIGHT;
+        aTask.L = Math.round(multiplier * GaussRand(expectationLenght, sigma));
+        aTask.number = i;
+        tasks.push(aTask);
+    }
+}
+
+function oneIterationStep() {
+    tasks.forEach(changePosition);
+    Draw();
+}
+
+function changePosition(task) {
+    task.x -= size;
+}
+
+function GaussRand(Mx, sigma) { // Box-Muller можно оптимизировать
     let firstRandom = 2 * Math.random() - 1;
     let secondRandom = 2 * Math.random() - 1;
     let r = firstRandom * firstRandom + secondRandom * secondRandom;
@@ -49,7 +73,12 @@ function GaussRand(Mx, sigma) { // Box-Muller
     }
 
         return Mx + sigma * firstRandom * Math.sqrt(-2 * Math.log(r) / r);
-    }
+}
+
+function expTime(lambda) {
+    
+    return -lambda * Math.log(1 - Math.random());
+}
     
 
 
